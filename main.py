3479ea0375e7ccdb7f9d0c2afba48c40a1dc9c94
Odinmylord <rgermenia@gmail.com>
2024-01-24 23:13:04 +0100

    started adding support for movement between floors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+from typing import List
+
 import pyrogram
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
@@ -10,7 +12,7 @@ import io
 cardinal_dict = {"N": 0, "E": 1, "S": 2, "W": 3}
 degrees_to_human = {0: "front", 90: "right", 180: "back", 270: "left"}
 
-floors = ["povo1_1", "povo1_0"]
+buildings = ["povo1"]
 
 keyboard_floors = InlineKeyboardMarkup(
     [
@@ -29,20 +31,21 @@ keyboard_floors = InlineKeyboardMarkup(
 
 
 class Graph:
-    def __init__(self, floor):
+    def __init__(self, building):
         self.nodes = []
-        with open(floor, "r") as f:
+        with open(building, "r") as f:
             data = json.load(f)
         for name in data:
             orientation = data[name]["orientation"]
             coords = data[name].get("coords")
+            floor = data[name].get("floor")
             if not orientation:
                 orientation = None
-            self.add_node(Node(name, orientation, coords))
+            self.add_node(Node(name, orientation, coords, floor))
         for name in data:
             for edge in data[name]["edges"]:
                 self.add_edge(name, Edge(self.get_by_name(edge[0]), edge[1], edge[2]))
-        self.floor = floor
+        self.building = building
 
     def add_node(self, node):
         self.nodes.append(node)
@@ -93,18 +96,18 @@ class Graph:
         if node is None:
             return None
         for edge in node.edges:
-            if not edge.destination.name.startswith(
-                "Cross"
-            ) and not edge.destination.name.startswith("Corridor"):
+            if (not edge.destination.name.startswith("Cross") and
+                    not edge.destination.name.startswith("Corridor")):
                 return edge.destination.name
         return None
 
 
 class Node:
-    def __init__(self, name, orientation, coords):
+    def __init__(self, name, orientation, coords, floor):
         self.name = name
         self.orientation = orientation
         self.coords = coords
+        self.floor = str(floor)
         self.edges = []
 
     def get_direction_to(self, destination):
@@ -129,58 +132,79 @@ def direction_converter(direction, new_direction):
     ]
 
 
-def prepare_image(graph, path):
-    file = "images/" + graph.floor.replace("json", "png")
-    if os.path.isfile(file):
-        with open(file, "rb") as f:
-            img = Image.open(f)
-            draw = ImageDraw.Draw(img)
-        # Draw a circle in the first point
-        draw.ellipse(
+def prepare_image(graph: Graph, path: List[Node]):
+    files = set()
+    building = graph.building.split(".")[0]
+    for node in path:
+        files.add("images/" + building + "_" + node.floor + ".png")
+    images = {}
+    for i, file in enumerate(files):
+        i = str(i)
+        if os.path.isfile(file):
+            with open(file, "rb") as f:
+                img = Image.open(f)
+                draw = ImageDraw.Draw(img)
+            images[i] = draw
+            images[i+"_img"] = img
+        else:
+            images[i] = None
+    print(images)
+    # Draw a circle in the first point
+    first_node_floor = path[0].floor
+    images[first_node_floor].ellipse(
+        (
+            path[0].coords[0] - 5,
+            path[0].coords[1] - 5,
+            path[0].coords[0] + 5,
+            path[0].coords[1] + 5,
+        ),
+        fill="red",
+    )
+    for i in range(len(path) - 1):
+        node_floor = path[i].floor
+        next_node_floor = path[i + 1].floor
+        if node_floor != next_node_floor:
+            continue
+        images[node_floor].line(
+            path[i].coords + path[i + 1].coords,
+            fill="red",
+            width=5,
+        )
+    last_node_floor = path[-1].floor
+    # draw a triangle in the last point
+    if path[-1].orientation in ["W", "E"]:
+        multiplier = 1 if path[-1].orientation == "E" else -1
+        images[last_node_floor].polygon(
             (
-                path[0].coords[0] - 5,
-                path[0].coords[1] - 5,
-                path[0].coords[0] + 5,
-                path[0].coords[1] + 5,
+                path[-1].coords[0], path[-1].coords[1] + (5*multiplier),
+                path[-1].coords[0] - 5, path[-1].coords[1],
+                path[-1].coords[0] + 5, path[-1].coords[1],
             ),
             fill="red",
         )
-        for i in range(len(path) - 1):
-            draw.line(
-                path[i].coords + path[i + 1].coords,
-                fill="red",
-                width=5,
-            )
-        # draw a triangle in the last point
-        if path[-1].orientation in ["W", "E"]:
-            multiplier = 1 if path[-1].orientation == "E" else -1
-            draw.polygon(
-                (
-                    path[-1].coords[0], path[-1].coords[1] + (5*multiplier),
-                    path[-1].coords[0] - 5, path[-1].coords[1],
-                    path[-1].coords[0] + 5, path[-1].coords[1],
-                ),
-                fill="red",
-            )
-        else:
-            multiplier = 1 if path[-1].orientation == "N" else -1
-            draw.polygon(
-                (
-                    path[-1].coords[0] + (5*multiplier), path[-1].coords[1],
-                    path[-1].coords[0], path[-1].coords[1] - 5,
-                    path[-1].coords[0], path[-1].coords[1] + 5,
-                ),
-                fill="red",
-            )
-        image = io.BytesIO()
-        img.save(image, format="PNG")
-        image.name = "image.png"
-        return image
+    else:
+        multiplier = 1 if path[-1].orientation == "N" else -1
+        images[last_node_floor].polygon(
+            (
+                path[-1].coords[0] + (5*multiplier), path[-1].coords[1],
+                path[-1].coords[0], path[-1].coords[1] - 5,
+                path[-1].coords[0], path[-1].coords[1] + 5,
+            ),
+            fill="red",
+        )
+    return_images = []
+    for i in images:
+        if i.endswith("_img"):
+            image = io.BytesIO()
+            images[i].save(image, format="PNG")
+            image.name = i + ".png"
+            return_images.append(image)
+    return return_images
 
 
 def pathfinder(graph: Graph, room1, room2):
     path = graph.dijkstra(graph.get_by_name(room1), graph.get_by_name(room2))
-    image = prepare_image(graph, path)
+    images = prepare_image(graph, path)
     steps = []
     path_dir = []
     # add direction to the nodes
@@ -210,7 +234,7 @@ def pathfinder(graph: Graph, room1, room2):
             output_string += direction + additional_string + "up to the corner."
         elif room.startswith("Corridor") and counter != len(steps) - 2:
             near_room = graph.get_room_from_corridor(room)
-            if counter:
+            if counter and near_room:
                 additional_string = "and go past " + near_room + "."
             else:
                 additional_string = "."
@@ -238,7 +262,7 @@ def pathfinder(graph: Graph, room1, room2):
             )
 
         counter += 1
-    return output_string, image
+    return output_string, images
 
 
 parser = configparser.ConfigParser()
@@ -252,8 +276,8 @@ app = pyrogram.Client(
 )
 
 graphs = {}
-for floor in floors:
-    graphs[floor] = Graph(floor=floor + ".json")
+for _building in buildings:
+    graphs[_building] = Graph(building=_building + ".json")
 
 
 @app.on_message(pyrogram.filters.command("start") & pyrogram.filters.private)
@@ -270,24 +294,31 @@ def nav(_, message):
 
 
 # check if callback is in floors with a filter
-floors_filter = pyrogram.filters.create(lambda _, __, query: query.data in floors)
+floors_filter = pyrogram.filters.create(lambda _, __, query: "_" in query.data and "$" not in query.data
+                                                             and query.data.split("_")[0] in buildings)
 
 
 @app.on_callback_query(floors_filter)
 def floor_callback(client, callback_query):
     # get floor from callback data
     floor = callback_query.data
+    building, floor_id = floor.split("_")
     # create graph
-    graph = Graph(floor=floor + ".json")
+    graph = Graph(building=building + ".json")
     # create keyboard
     keyboard = []
+    row = []
+    i = 0
     # add rooms to keyboard
     for node in graph.nodes:
-        if node.name.startswith("Corridor") or node.name.startswith("Cross"):
+        print(floor_id, node.floor, node.name)
+        if node.name.startswith("Corridor") or node.name.startswith("Cross") or node.floor != floor_id:
             continue
-        keyboard.append(
-            [InlineKeyboardButton(node.name, callback_data=floor + "$" + node.name)]
-        )
+        if i % 2 == 0 and i != 0:
+            keyboard.append(row)
+            row = []
+        row.append(InlineKeyboardButton(node.name, callback_data=floor + "$" + node.name))
+        i += 1
     keyboard = InlineKeyboardMarkup(keyboard)
     # send keyboard
     callback_query.edit_message_text("Choose a room", reply_markup=keyboard)
@@ -297,32 +328,36 @@ def room_filter(_, __, query):
     if query.data.count("$") != 1:
         return False
     floor, room = query.data.split("$")
-    node_names = [node.name for node in graphs[floor].nodes]
-    return floor in floors and room in node_names
+    building = floor.split("_")[0]
+    node_names = [node.name for node in graphs[building].nodes]
+    return building in buildings and room in node_names
 
 
 first_room_filter = pyrogram.filters.create(room_filter)
 
 
 @app.on_callback_query(first_room_filter)
-def first_room_callback(client, callback_query: pyrogram.types.CallbackQuery):
+def first_room_callback(_, callback_query: pyrogram.types.CallbackQuery):
     floor, room = callback_query.data.split("$")
-    print(floor, room)
+    building, floor_id = floor.split("_")
     keyboard = []
-    for node in graphs[floor].nodes:
+    row = []
+    i = 0
+    for node in graphs[building].nodes:
         if (
             node.name.startswith("Corridor")
             or node.name.startswith("Cross")
             or node.name == room
         ):
             continue
-        keyboard.append(
-            [
-                InlineKeyboardButton(
-                    node.name, callback_data=floor + "$" + room + "$" + node.name
-                )
-            ]
-        )
+        if i % 2 == 0 and i != 0:
+            keyboard.append(row)
+            row = []
+        row.append(InlineKeyboardButton(node.name, callback_data=floor + "$" + room + "$" + node.name))
+        i += 1
+    if row:
+        keyboard.append(row)
+    print(keyboard)
     keyboard = InlineKeyboardMarkup(keyboard)
     callback_query.edit_message_text(
         f"Starting point: {room}.\nChoose destination room", reply_markup=keyboard
@@ -334,8 +369,9 @@ def double_room_filter(_, __, query):
     if query.data.count("$") != 2:
         return False
     floor, room1, room2 = query.data.split("$")
-    node_names = [node.name for node in graphs[floor].nodes]
-    return floor in floors and room1 in node_names and room2 in node_names
+    building, floor_id = floor.split("_")
+    node_names = [node.name for node in graphs[building].nodes]
+    return building in buildings and room1 in node_names and room2 in node_names
 
 
 second_room_filter = pyrogram.filters.create(double_room_filter)
@@ -344,8 +380,10 @@ second_room_filter = pyrogram.filters.create(double_room_filter)
 @app.on_callback_query(second_room_filter)
 def second_room_callback(client, callback_query):
     floor, room1, room2 = callback_query.data.split("$")
-    path, image = pathfinder(graphs[floor], room1, room2)
-    callback_query.message.reply_photo(image, caption=path)
+    building, floor_id = floor.split("_")
+    path, images = pathfinder(graphs[building], room1, room2)
+    for image in images:
+        callback_query.message.reply_photo(image, caption=path)
     return True
 
 
